96297e0abed81582ee5bbcf420cdb9b8be48b660
dennis84 <d.dietrich84@gmail.com>
2014-05-11 09:33:24 +0200

    updated homepage

--- javascripts/views/page.js
+++ javascripts/views/page.js
@@ -1,7 +1,19 @@
 var Backbone = require('backbone')
 
 module.exports = Backbone.View.extend({
+  events: {
+    'click .btn-next-section': 'nextSection'
+  },
+
   render: function(template) {
     this.$el.html(template)
+  },
+
+  nextSection: function(e) {
+    e.preventDefault()
+    var elem = $(e.target).closest('section').next()
+    $('body').animate({
+      scrollTop: elem.offset().top
+    }, 200)
   }
 })
